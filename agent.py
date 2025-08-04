6b7d064c04564cfa987a600c9d46e6c78afdaab0
kamil <kamil.rahmatullin@gmail.com>
2025-08-04 18:49:23 +0300

    init

---
+++ agent.py
@@ -0,0 +1,95 @@
+from pendulum import Pendulum
+import random
+from collections import deque
+from model import Linear_QNet, QTrainer
+import numpy as np
+import torch
+from helper import plot
+
+max_memory = 100_000
+batch = 1000
+lr = 0.001
+
+class Agent():
+    def __init__(self):
+        self.n_sims = 0
+        self.epsilon = 0
+        self.gamma = 0.8
+        self.memory = deque(maxlen=max_memory)
+        self.model = Linear_QNet(8, 128, 2)
+        self.trainer = QTrainer(self.model, lr, self.gamma)
+    
+    def normalize(self, value, min_val, max_val):
+        return 2 * (value - min_val) / (max_val - min_val) - 1
+
+    def get_state(self, sim):
+        s = sim.stem
+        j = sim.joint
+        state = [
+            self.normalize(s.x, 90, 1110),
+            self.normalize(s.vec_x, -35.0, 35.0),
+            self.normalize(s.force_vec_x, -2.0, 2.0),
+            self.normalize(j.x, 90.0, 1110.0),
+            self.normalize(j.y, 120.0, 520.0),
+            self.normalize(j.vec_x, -35.0, 35.0),
+            self.normalize(j.vec_y, -35.0, 35.0),
+            self.normalize(j.force_vec_x, -10.0, 10.0)
+        ]
+        return np.array(state, dtype=np.float32)
+    
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    def train_lm(self):
+        if len(self.memory) > batch:
+            mini_sample = random.sample(self.memory, batch)
+        else:
+            mini_sample = self.memory
+        state, action, reward, next_state, done = zip(*mini_sample)
+        self.trainer.train_step(state, action, reward, next_state, done)
+    def train_sm(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        self.epsilon = 1000 - self.n_sims
+        final_move = [0, 0]
+        if random.randint(0, 500) < self.epsilon:
+            move = random.randint(0, 1)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
+    
+def train():
+    plot_time = []
+    plot_mean_time = []
+    total_time = 0
+    record = 0
+    agent = Agent()
+    sim = Pendulum()
+    while True:
+        state_old = agent.get_state(sim)
+        move = agent.get_action(state_old)
+        reward, done, time_up = sim.sim_step(move)
+        state_new = agent.get_state(sim)
+        agent.train_sm(state_old, move, reward, state_new, done)
+        agent.remember(state_old, move, reward, state_new, done)
+        if done:
+            sim.reset()
+            agent.n_sims += 1
+            agent.train_lm()
+            if time_up > record:
+                record = time_up
+                agent.model.save()
+            print("Sim", agent.n_sims, "Record: ", record)
+            plot_time.append(time_up)
+            total_time += time_up
+            mean_score = total_time/ agent.n_sims
+            plot_mean_time.append(mean_score)
+            plot(plot_time, plot_mean_time)
+
+if __name__ == "__main__":
+    train()
---
+++ human.py
@@ -0,0 +1,150 @@
+import pygame 
+from collections import namedtuple
+import math
+
+pygame.init()
+font = pygame.font.SysFont('arial', 25)
+
+Vector = namedtuple('Vector', 'x, y')
+
+class Point():
+    def __init__(self, x, y, vec_x, vec_y, force_vec_x, force_vec_y, fixed=False, tethered=False):
+        self.x = x
+        self.y = y
+        self.tethered = tethered
+        self.vec_x = vec_x
+        self.vec_y = vec_y
+        self.force_vec_x = force_vec_x
+        self.force_vec_y = force_vec_y
+        self.fixed = fixed
+
+    def move(self, force, w, h, gravity_vec, link=None, rad=0):
+        self.force_vec_x, self.force_vec_y = force
+        self.vec_x += self.force_vec_x + gravity_vec.x
+        self.vec_y += self.force_vec_y + gravity_vec.y
+
+        self.x += self.vec_x
+        self.y += self.vec_y
+
+        if self.tethered:
+
+            dx = self.x - link.x
+            dy = self.y - link.y
+            dist = math.hypot(dx, dy) + 1e-9
+            correction = (rad - dist) / dist
+            self.x += dx * correction
+            self.y += dy * correction
+
+            ux, uy  = dx/dist, dy/dist
+
+            k = 1
+            c = 1     
+            dlen = dist - rad
+
+            Fsx, Fsy = -k * dlen * ux, -k * dlen * uy
+            vrel_x = self.vec_x - force.x
+            vrel_y = self.vec_y - force.y
+            Fdamp_x = -c * (vrel_x*ux + vrel_y*uy) * ux
+            Fdamp_y = -c * (vrel_x*ux + vrel_y*uy) * uy
+
+            self.vec_x += (Fsx + Fdamp_x)
+            self.vec_y += (Fsy + Fdamp_y)
+
+        if self.y >= h / 2 and self.fixed:
+            self.y = h / 2
+            self.vec_y = 0
+        if self.y >= h - 30:
+            self.y = h - 30
+            self.vec_y = 0
+        if self.y <= 30:
+            self.y = 30
+            self.vec_y = 0
+        if self.x >= w - 90:
+            self.x = w - 90
+            self.vec_x = 0
+        if self.x <= 90:
+            self.x = 90
+            self.vec_x = 0
+
+        if self.vec_x > 45:
+            self.vec_x = 45
+        elif self.vec_x < -45:
+            self.vec_x = -45
+        if self.vec_y > 45:
+            self.vec_y = 45
+        elif self.vec_y < -45:
+            self.vec_y = -45
+
+class Pendulum():
+    def __init__(self, w=1200, h=640, speed=30, gravity=4):
+        self.w = w
+        self.h = h
+        self.stem_to_joint = 200
+        self.joint_to_leaf = 80
+        self.gravity = gravity
+        self.speed = speed
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Pendulum')
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+    def reset(self):
+        self.force = Vector(0, 0)
+        self.stem = Point(self.w / 2, self.h / 2, 0, 0, 0, 0, fixed=True)
+        self.joint = Point(self.w / 2, self.h / 2 + 200, 0, 0, 0, 0, tethered=True)
+        self.gravity_vec = Vector(0, self.gravity)
+        self.time_up = 0
+        self.frame_iteration = 0
+
+    def sim_step(self):
+        self.frame_iteration += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.force = Vector(-2, 0)
+                elif event.key == pygame.K_RIGHT:
+                    self.force = Vector(2, 0)
+
+        self.stem.move(self.force, self.w, self.h, self.gravity_vec)
+        self.joint.move(Vector(-self.stem.vec_x, -self.stem.vec_y), self.w, self.h, self.gravity_vec, link=Vector(self.stem.x, self.stem.y), rad=self.stem_to_joint)
+
+        self.reward = 0
+        self.done = False
+
+        if self.joint.y < self.h / 2:
+            self.reward = 5
+            self.time_up += 1
+        
+        if self.frame_iteration == 50:
+            self.done = True
+            return self.reward, self.done, self.time_up
+        
+        self._update_ui()
+        self.clock.tick(self.speed)
+
+        return self.reward, self.done, self.time_up
+    
+    def _update_ui(self):
+        self.display.fill((255, 255, 255))
+
+        pygame.draw.line(self.display, (0, 0, 0), (self.stem.x, self.stem.y), (self.joint.x, self.joint.y), 10)
+
+        pygame.draw.circle(self.display, (0, 0, 255), (self.stem.x, self.stem.y), 30)
+        pygame.draw.circle(self.display, (0, 255, 0), (self.joint.x, self.joint.y), 20)
+
+        text = font.render("Reward: " + str(self.reward) + " Time up: " + str(self.time_up) + "fvx: " + str(self.stem.force_vec_x) + "fvy: " + str(self.stem.force_vec_y), True, (0, 0, 0))
+        self.display.blit(text, [0, 0])
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    sim = Pendulum()
+
+    while True:
+        reward, done, time_up = sim.sim_step()
+
+    pygame.quit()
